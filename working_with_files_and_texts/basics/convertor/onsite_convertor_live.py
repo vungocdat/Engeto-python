9f92b0a8be686cb4489f642d385bfd6c91ea4d14
vungocdat <ngoc.dat.vu96@gmail.com>
2023-02-06 12:03:40 +0100

    not completed

--- working_with_files_and_texts/basics/convertor/onsite_convertor_live.py
+++ working_with_files_and_texts/basics/convertor/onsite_convertor_live.py
@@ -43,19 +43,36 @@ def find_all_text_files(path: str) -> List[str]:
 
 
 def read_text_file(fpath: str) -> List[str]:
-    ...
+    with open(fpath, mode="r", encoding="utf-8") as read_file:
+        return read_file.readlines()
 
 
 def process_data(data_lines: List[str]) -> List[str]:
-    ...
+    processed_data = []
+    for line in data_lines:
+        first_data = line.split(",")[0]
+        rest_data = line.split(",")[1:]
+        converted_first_data = conversion_dict[first_data]
+        converted_text = ",".join([converted_first_data] + rest_data)
+        processed_data.append(converted_text)
+    return processed_data
 
 
 def save_processed_data(processed_data: List[str], output_path: str):
-    ...
+    with open(output_path, mode="w", encoding="utf-8") as f:
+        output_str = "".join(processed_data)
+        f.write(output_str)
 
 
 def main(input_dir: str, output_dir: str):
-    ...
+    file_path = find_all_text_files(input_dir)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    for i, fpath in enumerate(file_path):
+        data_lines = read_text_file(fpath)
+        processed_data = process_data(data_lines)
+        output_path = os.path.join(output_dir, f"output_{i + 1}.txt")
+        save_processed_data(processed_data, output_path)
 
 
 if __name__ == "__main__":
